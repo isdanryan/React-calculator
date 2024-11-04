9cb4a0838db6ef1433b821f3d1b2860db8ef8505
Dan Ryan <isdanryan@gmail.com>
2024-11-04 21:56:27 +0000

    Create number buttons
    Create number buttons and functions.
    Handle state to prevent multiple "." and "0"
    Add all clear function for AC button

--- react-calculator/src/App.js
+++ react-calculator/src/App.js
@@ -1,29 +1,74 @@
+import { useReducer } from "react";
 import "./styles.css"
+import DigitButton from "./DigitButton";
+import OperationButton from "./OperationButton";
+
+export const ACTIONS = {
+  ADD_DIGIT: 'add-digit',
+  CLEAR: 'clear',
+  DELETE_DIGIT: 'delete-digit',
+  EQUALS: 'equals',
+}
+
+export const CHOOSE_OPERATION = {
+  ADD_DIGIT: 'add-digit',
+  CLEAR: 'clear',
+  DELETE_DIGIT: 'delete-digit',
+  EQUALS: 'equals',
+}
+
+const initialState = {
+  previousOperand: "",
+  currentOperand: "",
+  operation: "",
+};
+
+function reducer(state, { type, payload }) {
+  switch (type) {
+    case ACTIONS.ADD_DIGIT:
+      // Prevent multiple 0's being entered if starting 0 present
+      if (payload.digit === "0" && state.currentOperand === "0") {
+        return state
+      }
+      // Only allow one .
+      if (payload.digit === "." && state.currentOperand.includes(".")) {
+        return state
+      }
+      return {
+        ...state, // Copy existing state
+        currentOperand: `${state.currentOperand || ""}${payload.digit}`,
+      }
+    case ACTIONS.CLEAR:
+      return {}
+  }
+};
 
 function App() {
+  const [{ previousOperand, currentOperand, operation }, dispatch] = useReducer(reducer, initialState)
+
   return (
     <div className="calculator-grid">
       <div className="output">
-        <div className="previous-operand">123,234 *</div>
-        <div className="current-operand">123,234</div>
+        <div className="previous-operand">{previousOperand} {operation}</div>
+        <div className="current-operand">{currentOperand}</div>
       </div>
-      <button className="span-two">AC</button>
+      <button className="span-two" onClick={() => dispatch({ type: ACTIONS.CLEAR })}>AC</button>
       <button>DEL</button>
       <button>÷</button>
-      <button>1</button>
-      <button>2</button>
-      <button>3</button>
+      <DigitButton digit="1" dispatch={dispatch} />
+      <DigitButton digit="2" dispatch={dispatch} />
+      <DigitButton digit="3" dispatch={dispatch} />
       <button>*</button>
-      <button>4</button>
-      <button>5</button>
-      <button>6</button>
+      <DigitButton digit="4" dispatch={dispatch} />
+      <DigitButton digit="5" dispatch={dispatch} />
+      <DigitButton digit="6" dispatch={dispatch} />
       <button>+</button>
-      <button>7</button>
-      <button>8</button>
-      <button>9</button>
+      <DigitButton digit="7" dispatch={dispatch} />
+      <DigitButton digit="8" dispatch={dispatch} />
+      <DigitButton digit="9" dispatch={dispatch} />
       <button>-</button>
-      <button>.</button>
-      <button>0</button>
+      <DigitButton digit="." dispatch={dispatch} />
+      <DigitButton digit="0" dispatch={dispatch} />
       <button className="span-two">=</button>
     </div>
   );
